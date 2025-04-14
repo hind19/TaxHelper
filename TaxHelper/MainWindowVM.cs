a07db426055c9819e606ca198311995325307107
Nickolay Lychagin <lnvlad1974@gmail.com>
2025-04-14 23:46:41 +0300

    Add .csv processing

--- TaxHelper/MainWindowVM.cs
+++ TaxHelper/MainWindowVM.cs
@@ -1,10 +1,13 @@
 ﻿using Jellyfish;
+using Microsoft.Win32;
 using System.Collections.ObjectModel;
+using System.Configuration;
+using System.Globalization;
+using System.IO;
+using System.Windows;
 using TaxHelper.Common;
 using TaxHelper.Models;
 using TaxHelper.Services;
-
-
 
 namespace TaxHelper
 {
@@ -76,6 +79,67 @@ namespace TaxHelper
             TaxesResult = _taxCalculatorService.CalculateTax(Payments);
             Notify(nameof(TaxesResult));
             
+        });
+
+        public RelayCommand ImportCsvCommand => new RelayCommand((obj) =>
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = "CSV files (*.csv)|*.csv|All files (*.*)|*.*",
+                Title = "Выберите CSV файл"
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    var filePath = openFileDialog.FileName;
+                    var lines = File.ReadAllLines(filePath);
+
+                    if (lines.Length < 2)
+                    {
+                        throw new Exception("Файл пустой или содержит только заголовки.");
+                    }
+
+                    var headers = lines[0].Split(ConfigurationManager.AppSettings["ColumnSplitter"]);
+
+                    var dateIndex = Array.IndexOf(headers, ConfigurationManager.AppSettings["PaymentDateColumn"]);
+                    var sumIndex = Array.IndexOf(headers, ConfigurationManager.AppSettings["PaymentAmountColumn"]);
+                    var currencyIndex = Array.IndexOf(headers, ConfigurationManager.AppSettings["PaymentCurrencyColumn"]);
+
+                    if (dateIndex == -1 || sumIndex == -1 || currencyIndex == -1)
+                    {
+                        throw new Exception("Файл не содержит необходимых заголовков: PaymentDate, PaymentSum, PaymentCurrency.");
+                    }
+
+                    var importedPayments = new ObservableCollection<PaymentModel>();
+                    foreach (var line in lines.Skip(1)) 
+                    {
+                        var columns = line.Split(ConfigurationManager.AppSettings["ColumnSplitter"]);
+
+                        var paymentSum = double.Parse(columns[sumIndex], CultureInfo.InvariantCulture);
+                        if (paymentSum == 0)
+                        {
+                            continue;
+                        }
+
+                        var payment = new PaymentModel
+                        {
+                            PaymentDate = DateTime.Parse(columns[dateIndex]),
+                            PaymentSum = paymentSum,
+                            PaymentCurrency = Enum.Parse<Currencies>(columns[currencyIndex])
+                        };
+
+                        importedPayments.Add(payment);
+                    }
+
+                    Payments = importedPayments;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка при импорте CSV: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
         });
         #endregion
 
